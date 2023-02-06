6cc13910b7adef4d1b7cad9bbfbba157b2280f45
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-07 00:24:20 +0100

    Update Start.php

--- Start.php
+++ Start.php
@@ -14,7 +14,26 @@ class Start{
         $this->komentari=[];
         $this->proizvodi=[];
         $this->ocjene=[];
+        if($argc>1 && $argv[1]=='dev'){
+            $this->testPodaci();
+            $this->dev=true;
+        }else{
+            $this->dev=false;
+        }
+        $this->pozdravnaPoruka();
+        $this->glavniIzbornik();
     }
 
+    private function pozdravnaPoruka(){
+        echo 'Welcome to Terminal assigment - Web Shop' . PHP_EOL;
+    }
 
+    private function glavniIzbornik(){
+        echo 'Main menu' . PHP_EOL;
+        echo '1. Users' . PHP_EOL;
+        echo '2. Products' . PHP_EOL;
+        echo '3. Product rating' . PHP_EOL;
+        echo '4. Exit the program' . PHP_EOL;
+        $this->odabirOpcijeGlavniIzbornik();
+    }
 }
