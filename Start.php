c74f6a1a895bdeadb8eb903ff3cd17246166765a
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-06 23:41:33 +0100

    Update Start.php

--- Start.php
+++ Start.php
@@ -1,3 +1,20 @@
 <?php
 
 include_once 'Pomocno.php';
+
+class Start{
+    
+    private $korisnici;
+    private $komentari;
+    private $proizvodi;
+    private $ocjene;
+
+    public function __construct($argc,$argv){
+        $this->korisnici=[];
+        $this->komentari=[];
+        $this->proizvodi=[];
+        $this->ocjene=[];
+    }
+
+
+}
